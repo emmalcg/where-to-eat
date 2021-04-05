b2dcfe569d0b33046b2583a3558cd407e7eac47b
emma <emmalcgreen@gmail.com>
2021-04-04 22:41:23 -0400

    alphabetically sort restaurants

--- src/App.js
+++ src/App.js
@@ -26,6 +26,26 @@ function App() {
             foodTypes: restaurantData[restaurantKey].foodTypes,
           })
         }
+        
+        const sort = (property) => {
+          const sortOrder = 1;
+
+          if(property[0] ==='-') {
+            sortOrder = -1;
+            property = property.substr(1);
+          }
+
+          return function (a,b) {
+            if(sortOrder == -1) {
+              return b[property].localCompare(a[property]);
+            }else{
+              return a[property].localeCompare(b[property]);
+            }
+          }
+        }
+
+        restaurantList.sort(sort("name"));
+
         setRestaurants(restaurantList);
       })
     }, [])
